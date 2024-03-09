40bb9c73e6f4ac101e1034dbc3b6e5f76b9aa629
TianZonglin <1805984583@qq.com>
2024-03-09 15:56:51 +0800

    reupload

---
+++ rename.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+
+git filter-branch --env-filter '
+
+OLD_NAME="Glitch (glitch-hello-node)"
+
+CORRECT_NAME="TianZonglin"
+CORRECT_EMAIL="1805984583@qq.com"
+
+if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
